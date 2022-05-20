8f1f14c1662b5a454a3851137253f5be656e2c37
Jigar Joshi <jigarjm@gmail.com>
2022-05-20 15:41:37 -0700

    feat: Added readMany method in collection

--- src/tigris-collection.ts
+++
@@ -1,7 +0,0 @@
-export function TigrisCollection(
-    collectionName: string,
-): ClassDecorator {
-    return function (target) {
-            Reflect.defineProperty( target, 'tigris-collection-name', {value: collectionName});
-    }
-}
