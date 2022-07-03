4287b31f6a360613405b5c61066615b78e89e469
Jigar Joshi <jigarjm@gmail.com>
2022-07-02 17:27:59 -0700

    fix: Synced API changes

--- src/utility.ts
+++ src/utility.ts
@@ -16,6 +16,7 @@ import {
 	UpdateFieldsOperator
 } from "./types";
 import * as fs from "node:fs";
+import {FacetQueryFieldType, FacetQueryOptions, SearchRequestOptions} from "./search/types";
 
 export const Utility = {
 	stringToUint8Array(input: string): Uint8Array {
@@ -299,4 +300,17 @@ export const Utility = {
 	_base64Decode(b64String: string): string {
 		return Buffer.from(b64String, 'base64').toString('binary');
 	},
+
+	createFacetQueryOptions(options?: Partial<FacetQueryOptions>): FacetQueryOptions {
+		const defaults = {size: 10, type: FacetQueryFieldType.VALUE};
+		return {...defaults, ...options};
+	},
+
+	createSearchRequestOptions(options?: Partial<SearchRequestOptions>): SearchRequestOptions {
+		const defaults = {page: 1, perPage: 10};
+		return {
+			...defaults,
+			...options
+		};
+	}
 };
