8ca37c7d39d72b6be55907caf31105c0662e2dd3
Jigar Joshi <jigarjm@gmail.com>
2023-01-06 10:45:08 -0800

    feat: Implemented getSet method for cache

--- src/cache.ts
+++ src/cache.ts
@@ -1,10 +1,17 @@
-import { CacheDelResponse, CacheGetResponse, CacheSetOptions, CacheSetResponse } from "./types";
+import {
+	CacheDelResponse,
+	CacheGetResponse,
+	CacheGetSetResponse,
+	CacheSetOptions,
+	CacheSetResponse,
+} from "./types";
 import { CacheClient } from "./proto/server/v1/cache_grpc_pb";
 import {
 	DelRequest as ProtoDelRequest,
 	GetRequest as ProtoGetRequest,
 	KeysRequest as ProtoKeysRequest,
 	SetRequest as ProtoSetRequest,
+	GetSetRequest as ProtoGetSetRequest,
 } from "./proto/server/v1/cache_pb";
 import { Utility } from "./utility";
 import { TigrisClientConfig } from "./tigris";
@@ -27,6 +34,9 @@ export class Cache {
 		this._config = config;
 	}
 
+	/**
+	 * returns cache name
+	 */
 	public getCacheName(): string {
 		return this._cacheName;
 	}
@@ -36,6 +46,12 @@ export class Cache {
 	 * @param key
 	 * @param value
 	 * @param options - optionally set params.
+	 * @example
+	 * ```
+	 * const c1 = tigris.GetCache("c1);
+	 * const setResp = await c1.set("k1", "v1");
+	 * console.log(setResp.status);
+	 * ```
 	 */
 	public set(
 		key: string,
@@ -61,9 +77,6 @@ export class Cache {
 			if (options !== undefined && options.xx !== undefined) {
 				req.setXx(options.xx);
 			}
-			if (options !== undefined && options.get !== undefined) {
-				req.setGet(options.get);
-			}
 
 			this._cacheClient.set(req, (error, response) => {
 				if (error) {
@@ -76,8 +89,56 @@ export class Cache {
 	}
 
 	/**
+	 * Sets the key with value. And returns the old value (if exists)
+	 * @param key
+	 * @param value
+	 * @example
+	 * ```
+	 * const c1 = tigris.GetCache("c1);
+	 * const getSetResp = await c1.getSet("k1", "v1");
+	 * console.log(getSetResp.old_value);
+	 * ```
+	 */
+	public getSet(
+		key: string,
+		value: string | number | boolean | object
+	): Promise<CacheGetSetResponse> {
+		return new Promise<CacheGetSetResponse>((resolve, reject) => {
+			const req = new ProtoGetSetRequest()
+				.setProject(this._projectName)
+				.setName(this._cacheName)
+				.setKey(key)
+				.setValue(new TextEncoder().encode(Utility.objToJsonString(value as object)));
+
+			this._cacheClient.getSet(req, (error, response) => {
+				if (error) {
+					reject(error);
+				} else {
+					if (response.getOldValue() !== undefined && response.getOldValue_asU8().length > 0) {
+						resolve(
+							new CacheGetSetResponse(
+								response.getStatus(),
+								response.getMessage(),
+								Utility._base64DecodeToObject(response.getOldValue_asB64(), this._config)
+							)
+						);
+					} else {
+						resolve(new CacheGetSetResponse(response.getStatus(), response.getMessage()));
+					}
+				}
+			});
+		});
+	}
+
+	/**
 	 * get the value for the key, errors if the key doesn't exist or expired
 	 * @param key
+	 * @example
+	 * ```
+	 * const c1 = tigris.GetCache("c1);
+	 * const getResp = await c1.get("k1");
+	 * console.log(getResp.value);
+	 * ```
 	 */
 	public get(key: string): Promise<CacheGetResponse> {
 		return new Promise<CacheGetResponse>((resolve, reject) => {
@@ -101,6 +162,12 @@ export class Cache {
 	/**
 	 * deletes the key
 	 * @param key
+	 * @example
+	 * ```
+	 * const c1 = tigris.GetCache("c1);
+	 * const delResp = await c1.del("k1");
+	 * console.log(delResp.status);
+	 * ```
 	 */
 	public del(key: string): Promise<CacheDelResponse> {
 		return new Promise<CacheDelResponse>((resolve, reject) => {
@@ -120,6 +187,12 @@ export class Cache {
 	/**
 	 * returns an array of keys, complying the pattern
 	 * @param pattern - optional argument to filter keys
+	 * @example
+	 * ```
+	 * const c1 = tigris.GetCache("c1);
+	 * const keys = await c1.keys();
+	 * console.log(keys);
+	 * ```
 	 */
 	public keys(pattern?: string): Promise<string[]> {
 		return new Promise<string[]>((resolve, reject) => {
